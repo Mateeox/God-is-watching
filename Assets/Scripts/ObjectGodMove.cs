892c09252323402260c8b6cb8f9c2aa670d280c2
Mateusz Budzyński <mattibu@gmail.com>
2018-10-15 20:54:31 +0200

    Bounds for objects, jump change

--- Assets/Scripts/ObjectGodMove.cs
+++ Assets/Scripts/ObjectGodMove.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 public class ObjectGodMove : MonoBehaviour{
 
     public float objectsDistanceFromCamera;
+    public GameObject bound;
+    private Vector3 boundLeftUpCorner;
+    private Vector3 boundRightDownCorner;
     private bool isSelected;
     private Rigidbody rigidbody;
     private Vector3 point;
+
+    private void disableMoving()
+    {
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.black);
+
+        rigidbody.isKinematic = false;
+        this.isSelected = false;
+    }
 
     
     void Start()
@@ -16,18 +27,59 @@ public class ObjectGodMove : MonoBehaviour{
         isSelected = false;
         rigidbody = GetComponent<Rigidbody>();
         point = new Vector3();
-
+        boundLeftUpCorner = bound.transform.position + bound.transform.localScale / 2;
+        boundRightDownCorner = bound.transform.position - bound.transform.localScale / 2;
     }
     
     void Update()
     {
+        bool positionChanged = false;
         if (GameVariables.GameMode == GameVariables.GameModes.God && isSelected)
         {
             point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, objectsDistanceFromCamera));
-                       
-            transform.position = new Vector3(point.x, point.y, transform.position.z);
-           
+            point = new Vector3(point.x, point.y, transform.position.z);
+            positionChanged = true;
         }
+        else
+        {
+            point = transform.position;
+        }
+        
+        //Check if object is not out of bound
+        if (point.x > boundLeftUpCorner.x)
+        {
+            point.x = boundLeftUpCorner.x;
+            positionChanged = true;
+        }
+        else if (point.x < boundRightDownCorner.x)
+        {
+            point.x = boundRightDownCorner.x;
+            positionChanged = true;
+        }
+
+        if (point.y > boundLeftUpCorner.y)
+        {
+            point.y = boundLeftUpCorner.y;
+            positionChanged = true;
+        }
+        else if (point.y < boundRightDownCorner.y)
+        {
+            point.y = boundRightDownCorner.y;
+            positionChanged = true;
+        }
+
+        if (positionChanged)
+        {
+            transform.position = point;
+        }
+
+        //End moving object when player change mod to herose
+        if(GameVariables.GameMode == GameVariables.GameModes.God && Input.GetButtonDown("ChangeMode"))
+        {
+            disableMoving();
+        }
+
+
     }
 
     void OnMouseDown()
@@ -47,10 +99,7 @@ public class ObjectGodMove : MonoBehaviour{
         //disable object highligth on mouse button up
         if (GameVariables.GameMode == GameVariables.GameModes.God)
         {
-            GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.black);
-
-            rigidbody.isKinematic = false;
-            this.isSelected = false;
+            disableMoving();
         }
         
     }
