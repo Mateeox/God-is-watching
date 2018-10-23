6d1de49a5e41e6ed35c5dbb8b166fb69d143225c
Mateusz Łąpieś <mat.lapies@gmail.com>
2018-10-23 12:58:24 +0200

    Respawn after jumping into the water is working

--- Assets/Scripts/RespawnMill.cs
+++ Assets/Scripts/RespawnMill.cs
@@ -20,14 +20,13 @@ public class RespawnMill : MonoBehaviour {
         }
 	}
 
-    private void OnTriggerEnter(Collision collision)
+    private void OnTriggerEnter(Collider collider)
     {
-        if(collision.gameObject.name == "Player")
+        if(collider.gameObject.name == "Player")
         {
-            heroCam = collision.gameObject.GetComponent<Camera>();
-            heroCam.enabled = false;
-            collision.gameObject.transform.position = new Vector3(35.4f, 2.13f, -0.83f);
-            collision.gameObject.transform.rotation = new Quaternion(0, 0.7f, 0, 1.0f);
+            heroCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+            collider.gameObject.transform.position = new Vector3(35.4f, 2.13f, -0.83f);
+            collider.gameObject.transform.rotation = new Quaternion(0, 0.7f, 0, 1.0f);
         }
     }
 }
