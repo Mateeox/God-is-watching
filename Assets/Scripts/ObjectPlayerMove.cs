725880a7bfa63e0dacd79985c19bc5c7b948a901
Mateusz Łąpieś <mat.lapies@gmail.com>
2018-11-11 19:04:59 +0100

    Aktualizacja podnoszenia przedmiotów z pierwszej osoby
    Przedmioty stają się półprzezroczyste (materiał musi mieć ustawioną właściwość Transparent)
    Spotlight świeci ze środka przedmiotu na ziemie wskazująć miejsce upadku

--- Assets/Scripts/ObjectPlayerMove.cs
+++ Assets/Scripts/ObjectPlayerMove.cs
@@ -6,6 +6,7 @@ public class ObjectPlayerMove : MonoBehaviour {
 
     public static GameObject player;
     private Rigidbody rigidbody;
+    private GameObject spot;
 	// Use this for initialization
 	void Start () {
         player = GameObject.Find("Player");
@@ -17,6 +18,11 @@ public class ObjectPlayerMove : MonoBehaviour {
     {
         if (Input.GetButtonUp("PickUpObject") && Player.pickedObject == gameObject)
         {
+            DestroyImmediate(spot);
+            spot = null;
+            Color currColor = gameObject.GetComponent<MeshRenderer>().material.color;
+            currColor.a = 1.0f;
+            gameObject.GetComponent<MeshRenderer>().material.color = currColor;
             gameObject.transform.SetParent(null);
             rigidbody.isKinematic = false;
             Player.pickedObject = null;
@@ -32,13 +38,23 @@ public class ObjectPlayerMove : MonoBehaviour {
             if (Input.GetButtonDown("PickUpObject") && Player.pickedObject == null &&
                 Vector3.Distance(gameObject.transform.position, player.transform.position) < Player.maxPickUpDistance)
             {
-                
                 gameObject.transform.SetParent(player.transform);
                 rigidbody.isKinematic = true;
                 gameObject.transform.localPosition = new Vector3(0.25f, 0.25f, 1.5f);
                 Player.pickedObject = gameObject;
+                Color currColor = gameObject.GetComponent<MeshRenderer>().material.color;
+                currColor.a = 0.4f;
+                gameObject.GetComponent<MeshRenderer>().material.color = currColor;
+                spot = new GameObject("SpotLight");
+                Light lit = spot.AddComponent<Light>();
+                lit.range = 3f;
+                lit.type = LightType.Spot;
+                lit.intensity = 0.3f;
+                lit.color = new Color(229, 143, 33, 50f);
+                lit.transform.rotation = Quaternion.Euler(90.0f, 0, 0);
+                lit.transform.position = gameObject.transform.position;
+                spot.transform.parent = gameObject.transform;
             }           
-           
         }       
     }
 }
