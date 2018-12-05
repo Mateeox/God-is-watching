d80c20d2054c789c3cee70ce56007397696a9c76
Mateusz Łąpieś <mat.lapies@gmail.com>
2018-12-06 00:26:21 +0100

    DeathCam implemented
    When player dies, all the lights in the scene are beeing maxed out and camera is heading towards the checkpoint respawn

--- Assets/Scripts/FPP/Player.cs
+++ Assets/Scripts/FPP/Player.cs
@@ -109,7 +109,9 @@ public class Player : MonoBehaviour {
 		{
 				healthToMana();
 		}
-               
+
+        if (_deathAnimation)
+            AnimateDeath();
 		
 		//***********************************
 		//for debug only!
@@ -154,17 +156,43 @@ public class Player : MonoBehaviour {
 		if (healthBar.Value < 0.0001f)
 		{
             //player is dead...
-            Camera heroCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-            heroCam.enabled = false;
-            
+            _deathAnimation = true;
+            _heroCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+            _heroCam.transform.position += Vector3.up * 4.0f;
+            _startPos = _heroCam.transform.position;
             if (Checkpoint != null)
             {
                 Vector3 checkpointPos = Checkpoint.transform.position;
 				GlobalControl.Set = true;
                 GlobalControl.Position = new Vector3(checkpointPos.x, checkpointPos.y + 2.0f, checkpointPos.z + 2.0f);
                 GlobalControl.Rotation = new Quaternion(0, 0.7f, 0, 1.0f);
+                _lightList = FindObjectsOfType<Light>();
+                AnimateDeath();
             }
-            FullHealthMana();
+        }
+    }
+
+    private bool _deathAnimation = false;
+    private Light[] _lightList;
+    private float sign = -0.2f;
+    private float topIntensity = 15.0f;
+    private float lowIntensity = 0;
+    private float curIntensity = 5.0f;
+    private Camera _heroCam;
+    private Vector3 _startPos;
+    private void AnimateDeath()
+    {
+        foreach (Light light in _lightList)
+        {
+            if (light.intensity == lowIntensity)
+                sign *= -1.0f;
+            light.intensity += sign;                
+            curIntensity = light.intensity;
+        }
+        _heroCam.transform.LookAt(_startPos);
+        _heroCam.transform.position = Vector3.MoveTowards(_heroCam.transform.position, GlobalControl.Position, 15.0f * Time.deltaTime);
+        if(curIntensity > topIntensity) { 
+            _deathAnimation = false;
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
