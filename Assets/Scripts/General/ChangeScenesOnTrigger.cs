5924c230a96683b59b09c4ca231fe1eb663c6e38
TheFlyingKeyboard <theflyingkeyboard.net>
2018-12-18 11:19:38 +0100

    Added portal switching scenes on trigger, loading screen. Scene Portal for testing purposes

---
+++ Assets/Scripts/General/ChangeScenesOnTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeScenesOnTrigger : MonoBehaviour {
+	[SerializeField] private string playerTag;
+	[SerializeField] private string sceneName;
+
+	[SerializeField] private GameObject loadingMenu;
+	[SerializeField] private Slider sliderProgress;
+
+	private void OnTriggerEnter(Collider other){
+		if(other.CompareTag(playerTag)){
+			StartCoroutine(LoadAsync(sceneName));
+		}
+	}
+
+	IEnumerator LoadAsync(string scene){
+		loadingMenu.SetActive(true);
+
+		AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+
+		while(!operation.isDone){
+			sliderProgress.value = Mathf.Clamp01(operation.progress / 0.9f);
+
+			yield return null;
+		}
+	}
+}
