3ea6cdd802dab6bed16f26333d00b0b15e5162fa
TheFlyingKeyboard <theflyingkeyboard2@gmail.com>
2018-12-30 18:03:24 +0100

    Added Scenes Submenu to Main Menu, fixed menu screen scaling, scripted exit button

---
+++ Assets/Scripts/Menu/LoadSceneAsync.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAsync : MonoBehaviour {
+	[SerializeField] private GameObject loadingMenu;
+	[SerializeField] private Slider sliderProgress;
+
+	public void LoadScene(string name){
+		StartCoroutine(LoadAsync(name));
+	}
+	
+	IEnumerator LoadAsync(string scene){
+		loadingMenu.SetActive(true);
+
+		AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
+
+		while(!operation.isDone){
+			sliderProgress.value = Mathf.Clamp01(operation.progress / 0.9f);
+
+			yield return null;
+		}
+	}
+}
