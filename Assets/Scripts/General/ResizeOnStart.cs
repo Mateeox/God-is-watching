538f7b8b33285778bd0c96d0e0147d551e886f7c
TheFlyingKeyboard <theflyingkeyboard.net>
2019-01-05 16:12:04 +0100

    Teleport V2 - opening animation.

---
+++ Assets/Scripts/General/ResizeOnStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeOnStart : MonoBehaviour
+{
+	[SerializeField] 
+	private float desiredScale = 5.0f;
+
+	[SerializeField]
+	private float growFactor= 0.1f;
+
+    void Start()
+    {
+        StartCoroutine(Scale());
+    }
+ 
+    IEnumerator Scale()
+    {
+		while(desiredScale > transform.localScale.x)
+        {
+			transform.localScale += new Vector3(1, 1, 1) * Time.deltaTime * growFactor;
+			yield return null;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/General/TeleportOpener.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportOpener : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject objectToCheck;
+
+	[SerializeField]
+	private GameObject teleport;
+
+    void Update()
+    {
+		if(objectToCheck == null){
+			teleport.SetActive(true);
+		}
+    }
+}
