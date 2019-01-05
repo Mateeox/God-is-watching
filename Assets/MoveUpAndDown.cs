efc83fc2973b726731aa69d209d4ac507f97840b
TheFlyingKeyboard <theflyingkeyboard.net>
2019-01-05 18:44:55 +0100

    Teleport V3 - Panorama as texture, rotates and bounces.

---
+++ Assets/MoveUpAndDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpAndDown : MonoBehaviour
+{
+	[SerializeField]
+	private float minHeight = 0.5f;
+
+	[SerializeField]
+	private float maxHeight = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.localPosition = new Vector3(transform.localPosition.x, Mathf.PingPong(Time.time, (-minHeight) + maxHeight), transform.localPosition.z);
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 angles;
+
+	private void Update(){
+		transform.Rotate(angles * Time.deltaTime);
+	}
+}
