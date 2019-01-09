b848a8c5f88f8122ad67833c31ecb2a4000ee325
Maciek <maciek88888@gmail.com>
2019-01-09 22:14:24 +0100

    add clock - train riddle (no train model)

---
+++ Assets/Scripts/ObjectsBehaviour/Western/ClockTowerBehaviour.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClockTowerBehaviour : MonoBehaviour
+{
+	[SerializeField]
+	private float attachmentTime;
+	[SerializeField]
+	private float trainMovementTime;
+	[SerializeField]
+	private GameObject train;
+	private bool oneHandAttached = false;
+	private Vector3 finalPositionOnClock = new Vector3(-0.47f, 6.076f, 0.42f);
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+	{
+		
+    }
+	
+	public IEnumerator AttachHandToClock(Transform objectToMove, Vector3 newPosition, Quaternion newRotation, float time)
+	{
+		float elapsedTime = 0.1f;
+		Vector3 startingPos = objectToMove.localPosition;
+		Quaternion startingRotation = objectToMove.localRotation;
+		while (elapsedTime < time)
+		{
+			objectToMove.localPosition = Vector3.Lerp(startingPos, newPosition, (elapsedTime / time));
+			objectToMove.localRotation = Quaternion.Lerp(startingRotation , newRotation, (elapsedTime / time));
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+		Destroy(objectToMove.gameObject.GetComponent<ObjectGodMove>());
+		Destroy(objectToMove.gameObject.GetComponent<Rigidbody>());
+		objectToMove.localPosition = newPosition;
+		if (oneHandAttached)
+		{
+			Vector3 newPos = new Vector3(train.transform.position.x, train.transform.position.y, train.transform.position.z - 120.0f);
+			StartCoroutine(moveTrain(trainMovementTime, newPos));
+		}
+		else
+		{
+			oneHandAttached = true;
+		}
+	}
+	
+	private void OnTriggerEnter(Collider other)
+    {
+		//the magic numbers here are appropriate position/rotation coords of hands attached to the clock
+		if (other.CompareTag("ClockHandMinute"))
+		{
+			Quaternion newRotation = Quaternion.identity;
+			newRotation.eulerAngles = new Vector3(90.0f, 90.0f, 90.0f);
+			StartCoroutine(AttachHandToClock(other.transform, finalPositionOnClock, newRotation, attachmentTime));
+		}
+		
+		else if (other.CompareTag("ClockHandHour"))
+		{
+			Quaternion newRotation = Quaternion.identity;
+			newRotation.eulerAngles = new Vector3(0.0f, 270.0f, 270.0f);
+			StartCoroutine(AttachHandToClock(other.transform, finalPositionOnClock, newRotation, attachmentTime));
+		}
+	}
+	
+	public IEnumerator moveTrain(float time, Vector3 newPosition)
+	{
+		float elapsedTime = 0.1f;
+		Vector3 startingPos = train.transform.position;
+		while (elapsedTime < time)
+		{
+			train.transform.position = Vector3.Lerp(startingPos, newPosition, (elapsedTime / time));
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Scripts/ObjectsBehaviour/Western/MoveClockHands.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//yes, this class is nearly a copy of ObjectPlayerMove class, but I needed it without loosing info about original parent
+public class MoveClockHands : MonoBehaviour {
+
+    public static GameObject player;
+    private Rigidbody rigidbody;
+    private GameObject spot;
+    private bool flag;
+	private Transform originalParent;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        rigidbody = GetComponent<Rigidbody>();
+        flag = false;
+		originalParent = transform.parent;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonUp("PickUpObject") && Player.pickedObject == gameObject || flag)
+        {
+            flag = false;
+            DestroyImmediate(spot);
+            spot = null;
+            Color currColor = gameObject.GetComponent<MeshRenderer>().material.color;
+            currColor.a = 1.0f;
+            gameObject.GetComponent<MeshRenderer>().material.color = currColor;
+			gameObject.transform.SetParent(originalParent);
+            rigidbody.isKinematic = false;
+            Player.pickedObject = null;
+        }
+
+    }
+
+    void OnMouseOver()
+    {
+        if(GameVariables.GameMode == GameVariables.GameModes.Hero)
+        {
+           
+            if (Input.GetButtonDown("PickUpObject") && Player.pickedObject == null &&
+                Vector3.Distance(gameObject.transform.position, player.transform.position) < Player.maxPickUpDistance)
+            {
+                gameObject.transform.SetParent(player.transform);
+                rigidbody.isKinematic = true;
+                gameObject.transform.localPosition = new Vector3(0.25f, 0.25f, 1.5f);
+                Player.pickedObject = gameObject;
+                Color currColor = gameObject.GetComponent<MeshRenderer>().material.color;
+                currColor.a = 0.4f;
+                gameObject.GetComponent<MeshRenderer>().material.color = currColor;
+                spot = new GameObject("SpotLight");
+                Light lit = spot.AddComponent<Light>();
+                lit.range = 3f;
+                lit.type = LightType.Spot;
+                lit.intensity = 0.3f;
+                lit.color = new Color(229, 143, 33, 50f);
+                lit.transform.rotation = Quaternion.Euler(90.0f, 0, 0);
+                lit.transform.position = gameObject.transform.position;
+                spot.transform.parent = gameObject.transform;
+            }           
+        }       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        flag = true;
+        rigidbody.isKinematic = false;
+    }
+}
